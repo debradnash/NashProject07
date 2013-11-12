01acf99b876e227a8b0b640d3904c51eca699117
Debra Nash <dnash@smu.edu>
2013-11-12 13:23:50 -0600

    Updated main to add a new line before echoing line for print - seems to work

--- kernel.c
+++ kernel.c
@@ -18,28 +18,30 @@ int main()
 	// Step 1 Print to screen using Interrupt 0x10
     printString("This is Debras World - Hello \r\n\0");
 
-    printString("Huh? \r\n\0");
+    printString("Enter a line: \0");
+    printString("\n");
     readString(myLine);
+    printString("\n");
     printString(myLine);
 	while (1) ;
 		/* to do */
 	
 	return 0;
 }
-void readString(char* mystr) {
+void readString(char* myLine) {
  int idx = 0;
  char mychar =  0;
  while (1) {
   if (mychar == 0xD) break;
-  mychar = interrupt(0x16, 0, 0, 0, 0);
+  mychar = interrupt(0x16, mychar, 0, 0, 0);
   interrupt(0x10, 0xE*256 + mychar, 0, 0, 0);
-  mystr[idx]=mychar;
+  myLine[idx]=mychar;
   ++idx;
 
  }
- mystr[idx+1] = '\r';
- mystr[idx+2] = '\n';
- mystr[idx+3] = '\0';
+ myLine[idx+1] = '\r';
+ myLine[idx+2] = '\n';
+ myLine[idx+3] = '\0';
 }
 void printString(char* RandomWord) {
 
