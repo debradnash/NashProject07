4ccb2d07ef2f9d12c20ec84f7be824d134f2a33e
Debra Nash <dnash@smu.edu>
2013-11-17 11:09:14 -0600

    Adding source ..I'm stuck at the Step 3 of Project7

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ bcc -ansi -c kernel.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
-bochs -f opsys.bxrc
+#bochs -f opsys.bxrc
--- kernel.c
+++ kernel.c
@@ -1,3 +1,5 @@
+
+
 /* Nov 10 Project 7  
    Develop kernel to Print to Screen
                      Read from Keyboard
@@ -6,27 +8,67 @@
                      Provide Services
    Debra Nash CSE 7343
    */
-#include <stdio.h>
 
+
+
+int mod();
+int div();
 void printString();
 void readString();
 void readSector();
 
 char myLine[80] = 0;
-int main()
-{
-	// Step 1 Print to screen using Interrupt 0x10
-    printString("This is Debras World - Hello \r\n\0");
+char abuffer[512] = 0;
+int  sector = 30;
 
-    printString("Enter a line: \0");
-    printString("\n");
-    readString(myLine);
-    printString("\n");
-    printString(myLine);
-	while (1) ;
-		/* to do */
-	
-	return 0;
+int main(){
+ 
+ /*    printString("Hello World \0"); */
+
+/*     printString("Enter a new line \r\n\0");
+     readString(myLine);
+      
+     printString("\n");
+     printString(myLine);
+*/
+
+     readSector(abuffer, sector);  
+     printString(abuffer);
+     printString("\n");	
+
+ while (1);
+      return 0;
+}
+int div(int a, int b){
+  int q = 0;
+  while ((q * b) < a){
+    q = q + 1;
+  }
+  return q-1;
+}
+int mod(int dividend, int divisor){
+  while (dividend >= divisor){
+    dividend = dividend - divisor;
+  }
+}
+void readSector(char* abuffer, int sector){
+  int rs, h, t;
+  int idx = 0;
+  char mychar;
+  
+  /* rs = mod(sector,18)+ 1;*/
+  rs = 13;
+  h = 1;
+/*
+  h = div(sector,18);h = mod(h,2); */
+  t = 0;
+/*  t = div(sector, 36); Manually calculated variables for test*/
+  
+  mychar = abuffer[idx];
+  while (mychar != '\0') {
+    interrupt(0x13, 2*256+1, mychar, t*256+rs, h*256+0);
+    mychar = abuffer[++idx];
+  } 
 }
 void readString(char* myLine) {
  int idx = 0;
@@ -37,12 +79,12 @@ void readString(char* myLine) {
   interrupt(0x10, 0xE*256 + mychar, 0, 0, 0);
   myLine[idx]=mychar;
   ++idx;
-if (mychar == 0x8) {
+/* if (mychar == 0x8) {
     if (idx > 0)
        --idx;
     else
      continue;
-  }
+  } */
  }
  myLine[idx+1] = '\r';
  myLine[idx+2] = '\n';
